20e55a8f02fbb08e08b21c5b8d97ffdbb3250153
Henrique Nobre <henriquenobr@gmail.com>
2022-02-03 18:30:44 -0300

    api axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,33 +1,39 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
-export function TransactionsTable(){
-    return(
-        <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
+export function TransactionsTable() {
+  useEffect(() => {
+    api.get("transactions").then(response => console.log(response.data));
+  }, []);
 
-                <tbody>
-                    <tr>
-                        <td>Desenvolvimento site</td>
-                        <td className="income">R$12000,00</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2022</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="outcome">-R$1000,00</td>
-                        <td>Casa</td>
-                        <td>20/02/2022</td>
-                    </tr>
-                </tbody>
-            </table>
-        </Container>
-    )
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento site</td>
+            <td className="income">R$12000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="outcome">-R$1000,00</td>
+            <td>Casa</td>
+            <td>20/02/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
 }

2c30dbfaa91ccdf363ecc849b312270b3f06eac2
Henrique Nobre <henriquenobr@gmail.com>
2022-02-03 18:32:34 -0300

    error

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react'
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
+import Modal from 'react-modal'
 
 export function Header() {
+    const [isNewTransactionOpen, setIsNewTransactionOpen] = useState(false)
+
+    function handleOpenModal(){
+        setIsNewTransactionOpen(true)
+    }
+
+    function handleCloseModal(){
+        setIsNewTransactionOpen(false)
+    }
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="" />
-                <button>
+                <button onClick={handleOpenModal}>
                     Nova transação
                 </button>
             </Content>
